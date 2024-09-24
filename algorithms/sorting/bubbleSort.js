4a4b8b1c43b26549383ca93f339552f092e5fc82
Dell <prasuhirpara@gmail.com>
2024-09-24 09:50:09 +0530

    P bubble and selection sort algo

---
+++ algorithms/sorting/bubbleSort.js
@@ -0,0 +1,48 @@
+const arraySwap = require('../../utils/swap.js');
+
+/**
+    Sorts an array in the ascending or descending order in-place ( default : ascending )
+    * @param array Original array need to be sorted
+    * @param reverse Sorts an array in descending order
+    * @returns An array containing the array at each iteraton.
+*/
+
+// Stable
+// Time complexity: O(n * n)
+// Space complexity: O(1)
+
+const bubbleSort = (array, reverse = false) => {
+    if(array.length <= 1){
+        return array;
+    }
+
+    const steps = [];
+
+    for (let i = 0; i < array.length - 1; i++) {
+        let swapped = false;
+
+        for (let j = 0; j < array.length - i - 1; j++) {
+            if(!reverse){
+                if (array[j] > array[j + 1]) {
+                    arraySwap(array, j, j + 1);
+                    swapped = true;
+                }
+            }else{
+                if (array[j] < array[j + 1]) {
+                    arraySwap(array, j, j + 1);
+                    swapped = true;
+                }
+            }
+        }
+
+        steps.push([...array])
+
+        if (!swapped) {
+            break;
+        }
+    }
+
+    return steps;
+}
+
+module.exports = bubbleSort;
---
+++ algorithms/sorting/selectionSort.js
@@ -0,0 +1,49 @@
+const swap = require('../../utils/swap.js');
+
+/**
+ * Sorts an array via placing maximum element at the end in each iteration. (in-place)
+ * @param {Array} array Original array need to be sorted
+ * @param {boolean} reverse Sorts an array in descending order 
+ * @returns {Array} An array containing the array at each iteration.
+ */
+
+// Not Stable
+// Time complexity: O(n * n)
+// Space complexity: O(1)
+
+const selectionSort = (array, reverse = false) => {
+  if (array.length <= 1) {
+    return array;
+  }
+
+  const steps = [];
+
+  for (let i = 0; i < array.length - 1; i++) {
+    let max = i;
+    let min = i;
+
+    for (let j = 0; j < array.length - 1 - i; j++) {
+      if (!reverse) {
+        if (array[max] < array[j]) {
+          max = j;
+        }
+      } else {
+        if (array[min] > array[j]) {
+          min = j;
+        }
+      }
+    }
+
+    if (!reverse && max !== i) {
+      swap(array, array.length - i - 1, max);
+    } else if (reverse && min !== i) {
+      swap(array, i, min);
+    }
+
+    steps.push([...array]);
+  }
+
+  return steps;
+}
+
+module.exports = selectionSort;
