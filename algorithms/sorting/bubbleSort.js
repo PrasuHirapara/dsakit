788057766101eddc03682a149a89f569d475b526
Dell <prasuhirpara@gmail.com>
2024-09-24 10:55:40 +0530

    P cyclicSort algo

--- algorithms/sorting/bubbleSort.js
+++ algorithms/sorting/bubbleSort.js
@@ -16,12 +16,13 @@ const bubbleSort = (array, reverse = false) => {
         return array;
     }
 
+    const length = array.length
     const steps = [];
 
-    for (let i = 0; i < array.length - 1; i++) {
+    for (let i = 0; i < length - 1; i++) {
         let swapped = false;
 
-        for (let j = 0; j < array.length - i - 1; j++) {
+        for (let j = 0; j < length - i - 1; j++) {
             if(!reverse){
                 if (array[j] > array[j + 1]) {
                     arraySwap(array, j, j + 1);
