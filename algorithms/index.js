534e174e96421af0b71c5e6b6e8c975e98e1f5fb
Mh0pe22 <mohitbambharoliys2004@gmail.com>
2024-09-24 11:09:34 +0530

    M insertionSort

--- algorithms/index.js
+++ algorithms/index.js
@@ -1,5 +1,7 @@
 const bubbleSort = require("./sorting/bubbleSort");
-const countingSort = require("./sorting/countingSort");
+// const insertionSort = require("./sorting/insertionSort");
+// const countingSort = require("./sorting/countingSort");
+const insertionSort = require("./sorting/insertionSort");
 
 const arr = ["mohit" ,"prasu" , "prasu1" , "mohit2" ,"pinak" , "mihir" , 1,2 ,22]
 const arr2 = [1,2 ,22, "mohit" ,"prasu" ,423,4,223,34, "prasu1" , "mohit2" ,"pinak" , "mihir" ]
@@ -10,6 +12,8 @@ const temp = arr2.sort((a, b) => a - b);
 
 // console.log(temp);
 const arr3 = [3,4,5,6,1,2,8,2,3];
-const ans = countingSort(arr3);
-console.log(ans);
+insertionSort(arr3);
+console.log(arr3);
+
+// console.log(ans);
 
