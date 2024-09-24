ff8423ee9f6e635bba14fe11097897c515ddf796
Dell <prasuhirpara@gmail.com>
2024-09-24 11:15:48 +0530

    P modifying insertion sort

--- index.js
+++
@@ -1,7 +0,0 @@
-const selectionSort = require('./algorithms/sorting/selectionSort.js')
-const countingSort = require('./algorithms/sorting/cyclicSort.js')
-let arr = [4,5,3,2,1];
-
-const steps = countingSort(arr);
-console.log(arr,steps)
-
