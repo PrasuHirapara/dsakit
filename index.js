9efb0e3d172b7274fd70dc8f9cd836b366410376
Mh0pe22 <mohitbambharoliya2004@gmail.com>
2024-09-29 20:27:05 +0530

    M heap Sort

--- index.js
+++ index.js
@@ -11,5 +11,5 @@ const heapSort = require('./algorithms/sorting/heapSort.js');
 let arr = [5346,7,8,76,9807,3582,1,7,7,653,8,4,3,2,1];;
 
 heapSort(arr)
-// const steps = heapSort(arr , false , true);
+const steps = heapSort(arr , false , true);
 console.log(arr)
