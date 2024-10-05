7ee95e92fc9d2ba77222f2796efa3c0891f237e8
Dell <prasuhirpara@gmail.com>
2024-10-05 15:33:28 +0530

    P formatting index.js in DS folder

---
+++ dataStructures/index.js
@@ -0,0 +1,5 @@
+const linked_list = require('./linked list/index.js');
+
+module.exports = {
+    ...linked_list,
+}
--- test.js
+++ test.js
@@ -1,13 +1,13 @@
-const CircularLinkedList = require('./dataStructures/linked list/CircularLinkedList.js');
+const dsa = require('./index.js');
 
-let ll = new CircularLinkedList();
+let ll =new dsa.CircularLinkedList()
 
 ll.insert(2)
 ll.fromArray([3,4,5,6])
 ll.display();
 
 
-let ll2 = new CircularLinkedList();
+let ll2 = new dsa.CircularLinkedList();
 ll2.fromArray([7,8,9,10,11,12]);
 
 ll.addAll(ll2);
