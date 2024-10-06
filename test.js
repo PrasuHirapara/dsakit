fdea550e4a1c265468c043f7fc8808ebe442a6d7
Dell <prasuhirpara@gmail.com>
2024-10-06 19:10:34 +0530

    P Stack and DynamicStack ds

--- test.js
+++ test.js
@@ -1,15 +1,3 @@
 const dsa = require('./index.js');
 
 let arr = [36,56,16,-232456,913,3658,34,43,43,5,5,6,7,]
-
-const stack = new dsa.DynamicStack();
-stack.fromArray(arr);
-
-for(let i=0; i< 4; i++){
-    console.log(stack.pop());
-    
-}
-console.log(stack.peek());
-console.log("##########################");
-
-stack.display()
