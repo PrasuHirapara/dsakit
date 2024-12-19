b09fad1eec5900dbed5848ffeb97d6c168ff236b
Dell <prasuhirpara@gmail.com>
2024-12-19 16:35:56 +0530

    P AVLTree

--- dataStructures/tree/index.js
+++ dataStructures/tree/index.js
@@ -1,9 +1,11 @@
 const GeneralTree = require("./generalTree.js")
 const BinaryTree = require("./binaryTree");
 const BinarySearchTree = require("./binarySearchTree.js");
+const AVLTree = require("./AVL_Tree.js");
 
 module.exports = {
     GeneralTree,
     BinaryTree,
-    BinarySearchTree
+    BinarySearchTree,
+    AVLTree
 }
