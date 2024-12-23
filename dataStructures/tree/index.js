3be0c16e8645abd092453ff887e4f9ddb1d68085
Mh0pe22 <mohitbambharoliya2004@gmail.com>
2024-12-23 10:51:25 +0530

    M updated

--- dataStructures/tree/index.js
+++ dataStructures/tree/index.js
@@ -1,9 +1,13 @@
 const GeneralTree = require("./generalTree.js")
 const BinaryTree = require("./binaryTree");
 const BinarySearchTree = require("./binarySearchTree.js");
+const AVLTree = require("./AVL_Tree.js");
+const NArrayTree = require("./N_ary_tree.js");
 
 module.exports = {
+    AVLTree,
     GeneralTree,
     BinaryTree,
-    BinarySearchTree
+    BinarySearchTree,
+    NArrayTree
 }
