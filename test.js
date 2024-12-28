2df0d61a6f73b6667ae64b9592f6ffc8a997a4d1
Dell <prasuhirpara@gmail.com>
2024-12-28 20:04:33 +0530

    P dp

--- test.js
+++ test.js
@@ -3,4 +3,4 @@ const dsa = require("./index.js");
 const coins = [1, 2, 5];
 const amount = 11;
 
-console.log(dsa.minCoins(amount, coins)); // Output: 3 (5 + 5 + 1)
+console.log(dsa.minCoins(amount, coins));
